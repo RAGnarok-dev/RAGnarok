cf2bd46eaa9e4e1957dfeb076c8acd097169e81b
Xingkun <18646154381@163.com>
2025-03-31 10:04:26 +0800

    test: cache effectiveness

--- packages/ragnarok_toolkit/tests/test_permission.py
+++ packages/ragnarok_toolkit/tests/test_permission.py
@@ -8,12 +8,15 @@ test_manager = PermissionManager()
 
 
 @pytest.mark.asyncio
-async def test_register_fake_handler():
+async def test_cache():
+    time = 0
+
     async def fake_handler(sender: Any, access_token: str, knowledge_base_id: str) -> bool:
         await asyncio.sleep(1)
-        return True
+        return time % 2 == 0
 
     test_manager.register_permission_require_handler(fake_handler)
 
     assert await test_manager.check_permission("1", "1")
+    # should also be True, because of cache
     assert await test_manager.check_permission("1", "1")
